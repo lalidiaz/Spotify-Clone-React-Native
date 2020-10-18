9f8bcaac9d7e2e93023b63e6b774e7840ce74215
Lali <lauradiaz1586@gmail.com>
2020-10-18 17:02:44 +0400

    add amplify aws for the backend

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import { AppearanceProvider } from 'react-native-appearance';
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
-import PlayerWidget from './components/PlayeWidget';
+import { AppearanceProvider } from "react-native-appearance";
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import PlayerWidget from "./components/PlayeWidget";
 
-
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
